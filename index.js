511c1ec7ba31a8bea85386a04d734869c41a99b4
Phresh . <marquisej.galo@gmail.com>
2024-12-24 21:31:46 -0500

    Adding Questions

--- index.js
+++ index.js
@@ -4,7 +4,53 @@ const generateMarkdown=require("./utils/generateMarkdown")
 const inquirer=require("inquirer")
 // TODO: Create an array of questions for user input
 const questions = [
-   
+    {
+        type:"input",
+        message:"What is the title of your project",
+        name:"title",
+    }
+    ,
+    {
+        type:"list",
+        message:"Choose the following license:",
+        name:"license",
+        choices:["MIT","ISC"]
+    },
+    {
+        type: "input",
+        message: "Please provide a description of your project:",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What is the usage information?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "What are the contribution guidelines?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "What are the test instructions?",
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
 ];
 
 // TODO: Create a function to write README file
